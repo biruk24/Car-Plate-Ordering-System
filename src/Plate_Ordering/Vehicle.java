e8af34a2adde23fe8c48d8bbb71fd1407d8a1409
aymen197 <aymenmuhammed2@gmail.com>
2026-06-17 16:01:27 +0300

    update class

--- src/Plate_Ordering/Vehicle.java
+++ src/Plate_Ordering/Vehicle.java
@@ -1,4 +1,40 @@
 package Plate_Ordering;
 
 public class Vehicle {
+
+
+    public abstract class Vehicle {
+
+        protected String chassisNumber;
+        protected String formerPlate;
+        protected String ownerName;
+        protected VehicleCategory category;
+
+
+        public Vehicle(String chassisNumber, String formerPlate, String ownerName, VehicleCategory category) {
+            this.chassisNumber = chassisNumber;
+            this.formerPlate   = formerPlate;
+            this.ownerName     = ownerName;
+            this.category      = category;
+        }
+
+
+        public abstract String getVehicleInfo();
+
+        public String getChassisNumber() {
+            return chassisNumber;
+        }
+        public String getFormerPlate() {
+            return formerPlate;
+        }
+        public String getOwnerName() {
+            return ownerName;
+        }
+        public VehicleCategory getCategory() {
+            return category;
+        }
+        public int getPrice() {
+            return category.getPrice();
+        }
+    }
 }
