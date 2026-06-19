2b128d4834f48d307962cec9202f8c92f3fa4c9d
Amen Endriyas <amenendriyas@gmail.com>
2026-06-19 17:53:14 +0300

    setter + getter added

--- src/Plate_Ordering/PlateOrder.java
+++ src/Plate_Ordering/PlateOrder.java
@@ -2,12 +2,44 @@ package Plate_Ordering;
 
 public class PlateOrder {
     private String orderID;
-    private int plate_number;
+    private int plateNumber;
     private String userBuyer;
     private String targetVehicle;
+    private String Status;
 
-    public PlateOrder(String orderID, ){
+    public PlateOrder(String orderID, int plateNumber, String userBuyer, String targetVehicle, String Status){
+        this.orderID = orderID;
+        this.plateNumber = plateNumber;
+        this.userBuyer = userBuyer;
+        this.targetVehicle = targetVehicle;
+        this.Status = "pending";
 
     }
+
+    public String getrderID(){
+        return orderID;
+    }
+    public int getplatenumber(){
+        return plateNumber;
+    }
+    public void setplateNumber(int plateNumber){
+        this.plateNumber = plateNumber;
+    }
+    public String getuserBuyer(){
+        return userBuyer;
+    }
+    public String gettargetVehicle(){
+        return targetVehicle;
+    }
+    public String getStatus(){
+        return Status;
+    }
+    public void setStatus(String Status){
+        this.Status = Status;
+    }
+
+
+
+
 
 }
