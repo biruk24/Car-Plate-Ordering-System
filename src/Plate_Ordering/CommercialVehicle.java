bdb323694496a8876e78b3d0a6dcc85898e0588d
TANAN101 <abdibezu5@gmail.com>
2026-06-20 15:48:01 +0300

    created a commercialVehicle class , inherit from vehicle , creat constructor,override

--- src/Plate_Ordering/CommercialVehicle.java
+++ src/Plate_Ordering/CommercialVehicle.java
@@ -1,4 +1,19 @@
 package Plate_Ordering;
+public class CommercialVehicle extends Vehicle {
 
-public class CommercialVehicle {
+    private String businessName;
+
+    public CommercialVehicle(String chassisNumber, String formerPlate,
+                             String ownerName, VehicleCategory category, String businessName) {
+        super(chassisNumber, formerPlate, ownerName, category);
+        this.businessName = businessName;
+    }
+
+    @Override
+    public String getVehicleInfo() {
+        return "Commercial Vehicle | Business: " + businessName + " | Owner: " + ownerName;
+    }
+
+    public String getBusinessName() { return businessName; }
 }
+
