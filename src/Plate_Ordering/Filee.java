e2d38461e7e45821dfd81e6b24e579c0ab3e94f1
TANAN101 <abdibezu5@gmail.com>
2026-06-21 22:00:19 +0300

    method that creats file for plate order , loading orders , saves receipt

--- src/Plate_Ordering/Filee.java
+++ src/Plate_Ordering/Filee.java
@@ -53,4 +53,62 @@ public class Filee {
         return list;
     }
 
+    public static void saveOrders(List<PlateOrder> orders) {
+        try {
+            FileWriter fw = new FileWriter(ORDERS_FILE);
+            BufferedWriter bw = new BufferedWriter(fw);
+
+            for (PlateOrder o : orders) {
+                bw.write(o.toFileLine());
+                bw.newLine();
+            }
+
+            bw.close();
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error saving orders: " + e.getMessage());
+        }
+    }
+
+
+    public static List<PlateOrder> loadOrders() {
+        List<PlateOrder> list = new ArrayList<>();
+        File file = new File(ORDERS_FILE);
+
+        if (!file.exists()) return list;
+
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (!line.trim().isEmpty()) {
+                    PlateOrder o = PlateOrder.fromFileLine(line);
+                    if (o != null) list.add(o);
+                }
+            }
+
+            br.close();
+            fr.close();
+        } catch (IOException e) {
+            System.out.println("Error loading orders: " + e.getMessage());
+        }
+
+        return list;
+    }
+
+    public static void saveReceipt(String content, String fileName) {
+        try {
+            FileWriter fw = new FileWriter(fileName);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write(content);
+            bw.close();
+            fw.close();
+            System.out.println("Receipt saved as: " + fileName);
+        } catch (IOException e) {
+            System.out.println("Error saving receipt: " + e.getMessage());
+        }
+    }
 }
+
