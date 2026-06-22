8a15439113ce4da21f130cfc0d63edd1f759404d
Amen Endriyas <amenendriyas@gmail.com>
2026-06-22 19:06:32 +0300

    plate order done.

--- src/Plate_Ordering/PlateOrder.java
+++ src/Plate_Ordering/PlateOrder.java
@@ -4,14 +4,14 @@ public class PlateOrder {
     private String orderID;
     private int plateNumber;
     private User userBuyer;
-    private String targetVehicle;
+    private Vehicle Vehicletype;
     private String Status;
 
-    public PlateOrder(String orderID, int plateNumber, User userBuyer, String targetVehicle, String Status){
+    public PlateOrder(String orderID, int plateNumber, User userBuyer, Vehicle Vehicletype, String Status){
         this.orderID = orderID;
         this.plateNumber = plateNumber;
         this.userBuyer = userBuyer;
-        this.targetVehicle = targetVehicle;
+        this.Vehicletype = Vehicletype;
         this.Status = "pending";
 
     }
@@ -28,8 +28,8 @@ public class PlateOrder {
     public User getuserBuyer(){
         return userBuyer;
     }
-    public String gettargetVehicle(){
-        return targetVehicle;
+    public Vehicle getVehicletype(){
+        return Vehicletype;
     }
     public String getStatus(){
         return Status;
@@ -44,14 +44,15 @@ public class PlateOrder {
         System.out.println("STATUS: " + Status);
         System.out.println("REQUESTED PLATE: " + plateNumber);
         System.out.println("CUSTOMER: " + userBuyer.getFullName());
-        System.out.println("VEHICLE: " + targetVehicle );
+        System.out.println("VEHICLE: " + Vehicletype.getCategory());
         System.out.println("=============================");
     }
 
     public static void main(String[] args) {
         User cust = new User("abc", "14qw", "kebe", "0914257861", "buy");
+        
 
-        PlateOrder order = new PlateOrder("1441", 5545, cust, "nissan", "pending");
+        PlateOrder order = new PlateOrder(null, 0, cust, null, null);
 
         order.PlateOrderDisplay();
     }
